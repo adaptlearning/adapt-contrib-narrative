33b7d1f8680425785a3a9ead920b6e24776b3dcf
Daryl Hedley <darylhedley@hotmail.com>
2014-01-16 09:57:41 +0000

    Removed console.log

--- js/adapt-contrib-narrative.js
+++ js/adapt-contrib-narrative.js
@@ -244,7 +244,6 @@ define(function(require) {
 
         evaluateCompletion: function() {
             if (this.$('.visited').length == this.model.get('_itemCount')) {
-                console.log('complete');
                 this.setCompletionStatus();
             }
         },
