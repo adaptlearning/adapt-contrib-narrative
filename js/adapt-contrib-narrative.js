cd2e8de82cd606b455886d2866d7a6325ee6dad1
Kev Adsett <kev.adsett@gmail.com>
2014-01-27 18:49:33 +0000

    Alphabetised functions

--- js/adapt-contrib-narrative.js
+++ js/adapt-contrib-narrative.js
@@ -19,51 +19,6 @@ define(function(require) {
             this.$('.narrative-strapline-header-inner').stop(true, true).animate({'margin-left': -(strapLineSize * itemIndex)});
         },
         
-        events: {
-            'touchstart .narrative-slider':'onTouchNavigationStarted',
-            'click .narrative-popup-open':'openPopup',
-            'click .narrative-popup-close':'closePopup',
-            'click .narrative-controls':'onNavigationClicked'
-        },
-
-        preRender: function () {
-            this.listenTo(Adapt, 'device:changed', this.reRender, this);
-            this.listenTo(Adapt, 'device:resize', this.resizeControl, this);
-            this.setDeviceSize();
-        },
-
-        setDeviceSize: function() {
-            if (Adapt.device.screenSize === 'large') {
-                this.$el.addClass('desktop').removeClass('mobile');
-                this.model.set('_isDesktop', true);
-            } else {
-                this.$el.addClass('mobile').removeClass('desktop');
-                this.model.set('_isDesktop', false)
-            }
-        },
-
-        postRender: function() {
-            this.$('.narrative-slider').imageready(_.bind(function(){
-                this.setReadyStatus();
-            }, this));
-            this.setupNarrative();
-        }, 
-
-        setupNarrative: function() {
-            _.bindAll(this, 'onTouchMove', 'onTouchEnd');
-            this.setDeviceSize();
-            this.model.set('_itemCount', this.model.get('items').length);
-
-            this.model.set('_active', true);
-
-            if (this.model.get('_stage')) {
-                this.setStage(this.model.get('_stage'));
-            } else {
-                this.setStage(0);
-            }
-            this.calculateWidths();
-        },
-
         calculateWidths: function() {
             var slideWidth = this.$('.narrative-slide-container').width();
             var slideCount = this.model.get('_itemCount');
@@ -74,7 +29,7 @@ define(function(require) {
             this.$('.narrative-slider-graphic').width(slideWidth)
             this.$('.narrative-strapline-header').width(slideWidth);
             this.$('.narrative-strapline-title').width(slideWidth);
-            
+
             this.$('.narrative-slider').width(fullSlideWidth);
             this.$('.narrative-strapline-header-inner').width(fullSlideWidth);
 
@@ -86,6 +41,16 @@ define(function(require) {
             this.model.set('_finalItemLeft', fullSlideWidth - slideWidth);
         },
 
+        closePopup: function (event) {
+            event.preventDefault();
+            this.model.set('_active', true);
+
+            this.$('.narrative-popup-close').blur();
+            this.$('.narrative-popup').addClass('narrative-hidden');
+            
+            this.evaluateCompletion();
+        },
+        
         constrainStage: function(stage) {
             if (stage > this.model.get('items').length - 1) {
                 stage = this.model.get('items').length - 1;
@@ -107,6 +72,41 @@ define(function(require) {
             return newLeft;
         },
         
+        evaluateCompletion: function() {
+            if (this.getVisitedItems().length == this.model.get('items').length) {
+                this.setCompletionStatus();
+            }
+        },
+
+        evaluateNavigation: function() {
+            var currentStage = this.model.get('_stage');
+            var itemCount = this.model.get('_itemCount');
+
+            if (currentStage == 0) {
+                this.$('.narrative-control-left').addClass('narrative-hidden');
+
+                if (itemCount > 1) {
+                    this.$('.narrative-control-right').removeClass('narrative-hidden');
+                }
+            } else {
+                this.$('.narrative-control-left').removeClass('narrative-hidden');
+
+                if (currentStage == itemCount - 1) {
+                    this.$('.narrative-control-right').addClass('narrative-hidden');
+                } else {
+                    this.$('.narrative-control-right').removeClass('narrative-hidden');
+                }
+            }
+
+        },
+
+        events: {
+            'touchstart .narrative-slider':'onTouchNavigationStarted',
+            'click .narrative-popup-open':'openPopup',
+            'click .narrative-popup-close':'closePopup',
+            'click .narrative-controls':'onNavigationClicked'
+        },
+
         getNearestItemIndex: function() {
             var currentPosition = parseInt(this.$('.narrative-slider').css('margin-left')),
                 graphicWidth = this.$('.narrative-slider-graphic').width(),
@@ -123,6 +123,12 @@ define(function(require) {
             return this.constrainStage(stage);
         },
         
+        getVisitedItems: function() {
+          return _.filter(this.model.get('items'), function(item) {
+            return item.visited;
+          });
+        },
+
         moveElement: function($element, deltaX) {
             var previousLeft = parseInt($element.css('margin-left')),
                 newLeft = previousLeft + deltaX;
@@ -132,6 +138,39 @@ define(function(require) {
             $element.css('margin-left', newLeft + 'px');
         },
         
+        openPopup: function (event) {
+            event.preventDefault();
+            this.model.set('_active', false);
+
+            var outerMargin = parseFloat(this.$('.narrative-popup-inner').css('margin'));
+            var innerPadding = parseFloat(this.$('.narrative-popup-inner').css('padding'));
+            var toolBarHeight = this.$('.narrative-toolbar').height();
+
+            this.$('.narrative-slider-graphic').eq(this.model.get('_stage')).addClass('visited');
+            this.$('.narrative-popup-toolbar-title').addClass('narrative-hidden').eq(this.model.get('_stage')).removeClass('narrative-hidden');
+            this.$('.narrative-popup-content').addClass('narrative-hidden').eq(this.model.get('_stage')).removeClass('narrative-hidden');
+            this.$('.narrative-popup-inner').css('height', $(window).height() - (outerMargin * 2) - (innerPadding * 2));
+            this.$('.narrative-popup').removeClass('narrative-hidden');
+            this.$('.narrative-popup-content').css('height', (this.$('.narrative-popup-inner').height() - toolBarHeight));
+        },
+
+        onNavigationClicked: function(event) {
+            event.preventDefault();
+            
+            if (!this.model.get('_active')) return;
+            
+            var stage = this.model.get('_stage'),
+                numberOfItems = this.model.get('_itemCount');
+            
+            if ($(event.currentTarget).hasClass('narrative-control-right')) {
+                stage++;
+            } else if ($(event.currentTarget).hasClass('narrative-control-left')) {
+                stage--;
+            }
+            stage = (stage + numberOfItems) % numberOfItems;
+            this.setStage(stage);
+        },
+
         onTouchNavigationStarted: function(event) {
             event.preventDefault();
             if (!this.model.get('_active')) return;
@@ -164,6 +203,26 @@ define(function(require) {
             this.model.set('_currentX', currentX);
         },
         
+        prepareHotgraphicModel: function() {
+          var model = this.model;
+          model.set('_component', 'hotgraphic');
+          model.set('body', model.get('originalBody'));
+          return model;
+        },
+
+        preRender: function () {
+            this.listenTo(Adapt, 'device:changed', this.reRender, this);
+            this.listenTo(Adapt, 'device:resize', this.resizeControl, this);
+            this.setDeviceSize();
+        },
+
+        postRender: function() {
+            this.$('.narrative-slider').imageready(_.bind(function(){
+                this.setReadyStatus();
+            }, this));
+            this.setupNarrative();
+        }, 
+
         resizeControl: function() {
             this.setDeviceSize();
             this.calculateWidths();
@@ -185,28 +244,14 @@ define(function(require) {
             this.remove();
         },
 
-        prepareHotgraphicModel: function() {
-          var model = this.model;
-          model.set('_component', 'hotgraphic');
-          model.set('body', model.get('originalBody'));
-          return model;
-        },
-
-        onNavigationClicked: function(event) {
-            event.preventDefault();
-            
-            if (!this.model.get('_active')) return;
-            
-            var stage = this.model.get('_stage'),
-                numberOfItems = this.model.get('_itemCount');
-            
-            if ($(event.currentTarget).hasClass('narrative-control-right')) {
-                stage++;
-            } else if ($(event.currentTarget).hasClass('narrative-control-left')) {
-                stage--;
+        setDeviceSize: function() {
+            if (Adapt.device.screenSize === 'large') {
+                this.$el.addClass('desktop').removeClass('mobile');
+                this.model.set('_isDesktop', true);
+            } else {
+                this.$el.addClass('mobile').removeClass('desktop');
+                this.model.set('_isDesktop', false)
             }
-            stage = (stage + numberOfItems) % numberOfItems;
-            this.setStage(stage);
         },
 
         setStage: function(stage) {
@@ -227,64 +272,19 @@ define(function(require) {
             this.animateSliderToIndex(stage);
         },
 
-        evaluateNavigation: function() {
-            var currentStage = this.model.get('_stage');
-            var itemCount = this.model.get('_itemCount');
+        setupNarrative: function() {
+            _.bindAll(this, 'onTouchMove', 'onTouchEnd');
+            this.setDeviceSize();
+            this.model.set('_itemCount', this.model.get('items').length);
 
-            if (currentStage == 0) {
-                this.$('.narrative-control-left').addClass('narrative-hidden');
-
-                if (itemCount > 1) {
-                    this.$('.narrative-control-right').removeClass('narrative-hidden');
-                }
-            } else {
-                this.$('.narrative-control-left').removeClass('narrative-hidden');
-
-                if (currentStage == itemCount - 1) {
-                    this.$('.narrative-control-right').addClass('narrative-hidden');
-                } else {
-                    this.$('.narrative-control-right').removeClass('narrative-hidden');
-                }
-            }
-
-        },
-
-        getVisitedItems: function() {
-          return _.filter(this.model.get('items'), function(item) {
-            return item.visited;
-          });
-        },
-
-        evaluateCompletion: function() {
-            if (this.getVisitedItems().length == this.model.get('items').length) {
-                this.setCompletionStatus();
-            }
-        },
-
-        openPopup: function (event) {
-            event.preventDefault();
-            this.model.set('_active', false);
-
-            var outerMargin = parseFloat(this.$('.narrative-popup-inner').css('margin'));
-            var innerPadding = parseFloat(this.$('.narrative-popup-inner').css('padding'));
-            var toolBarHeight = this.$('.narrative-toolbar').height();
-
-            this.$('.narrative-slider-graphic').eq(this.model.get('_stage')).addClass('visited');
-            this.$('.narrative-popup-toolbar-title').addClass('narrative-hidden').eq(this.model.get('_stage')).removeClass('narrative-hidden');
-            this.$('.narrative-popup-content').addClass('narrative-hidden').eq(this.model.get('_stage')).removeClass('narrative-hidden');
-            this.$('.narrative-popup-inner').css('height', $(window).height() - (outerMargin * 2) - (innerPadding * 2));
-            this.$('.narrative-popup').removeClass('narrative-hidden');
-            this.$('.narrative-popup-content').css('height', (this.$('.narrative-popup-inner').height() - toolBarHeight));
-        },
-
-        closePopup: function (event) {
-            event.preventDefault();
             this.model.set('_active', true);
 
-            this.$('.narrative-popup-close').blur();
-            this.$('.narrative-popup').addClass('narrative-hidden');
-            
-            this.evaluateCompletion();
+            if (this.model.get('_stage')) {
+                this.setStage(this.model.get('_stage'));
+            } else {
+                this.setStage(0);
+            }
+            this.calculateWidths();
         }
     });
     
