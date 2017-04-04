2c74ef1a109b810336697941a9ddd29a0961e1f5
Thomas Berger <thomas.berger@learnchamp.com>
2017-04-04 13:51:51 +0200

    use Model and View

---
+++ js/narrativeModel.js
@@ -0,0 +1,12 @@
+define([
+    'core/js/adapt',
+    'core/js/models/itemsModel'
+], function(Adapt, ItemsModel) {
+
+    var NarrativeModel = ItemsModel.extend({
+        
+    });
+
+    return NarrativeModel;
+
+});
