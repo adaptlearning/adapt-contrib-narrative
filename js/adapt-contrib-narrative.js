b80e6ef4464b638230a2b1b3ec34a2cb8ec9f9d9
Guy Willis <guywillis@users.noreply.github.com>
2019-11-13 16:25:14 +0000

    Release/v5 (#196)
    * Converted tabs to spaces, indentation amends
    
    * Naming refactor, less refactor
    
    * Typo fix, added comment, icon definition amend, removed redundant clearfix
    
    * Button class update
    
    * Variable name amend
    
    * Strapline focus fix
    
    * Removed redundant classes
    
    * Attribution support
    
    * Bower, schema, example and readme updates
    
    * Removed unnecessary float, EM to REM conversion
    
    * Updated license and cross platform coverage

--- js/adapt-contrib-narrative.js
+++ js/adapt-contrib-narrative.js
@@ -1,12 +1,12 @@
 define([
-    'core/js/adapt',
-    './narrativeView',
-    'core/js/models/itemsComponentModel'
+  'core/js/adapt',
+  './narrativeView',
+  'core/js/models/itemsComponentModel'
 ], function(Adapt, NarrativeView, ItemsComponentModel) {
 
-    return Adapt.register('narrative', {
-        model: ItemsComponentModel,
-        view: NarrativeView
-    });
+  return Adapt.register('narrative', {
+    model: ItemsComponentModel,
+    view: NarrativeView
+  });
 
 });
