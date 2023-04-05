2909174314b929f55aebf661607e29833e4690af
Oliver Foster <oliver.foster@kineo.com>
2023-04-05 09:06:10 +0100

    Fix: Narrative item header level calculation

--- templates/narrative.jsx
+++ templates/narrative.jsx
@@ -6,6 +6,7 @@ import { templates, compile, classes } from 'core/js/reactHelpers';
 export default function Narrative(props) {
 
   const {
+    _id,
     _items,
     _translateXOffset,
     _hasNavigationInTextArea,
@@ -90,7 +91,7 @@ export default function Narrative(props) {
                   <div
                     className="narrative__content-title-inner"
                     role="heading"
-                    aria-level={a11y.ariaLevel({ level: 'componentItem', override: (_ariaLevel || null) })}
+                    aria-level={a11y.ariaLevel({ id: _id, level: 'componentItem', override: (_ariaLevel || null) })}
                     dangerouslySetInnerHTML={{ __html: compile(title, props) }} />
                 </div>
                 }
