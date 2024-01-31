b8367cf9110a8db1788666993f664a99bb0ffab7
Oliver Foster <oliver.foster@kineo.com>
2024-01-31 12:27:01 +0000

    Fix: Keep active item position on save/restore (fixes #295)

--- js/NarrativeModel.js
+++ js/NarrativeModel.js
@@ -1,3 +1,24 @@
 import ItemsComponentModel from 'core/js/models/itemsComponentModel';
 
-export default class NarrativeModel extends ItemsComponentModel {}
+export default class NarrativeModel extends ItemsComponentModel {
+  restoreUserAnswers() {
+    const numberArray = this.get('_userAnswer');
+    if (!numberArray) return;
+    const children = this.getChildren();
+    const shouldRestoreActiveItem = (numberArray.length > children.length);
+    if (shouldRestoreActiveItem) {
+      const activeItemIndex = numberArray.pop();
+      this.setActiveItem(activeItemIndex);
+    }
+    children.forEach(child => child.set('_isVisited', Boolean(numberArray[child.get('_index')])));
+  }
+
+  storeUserAnswer() {
+    const items = this.getChildren().slice(0);
+    items.sort((a, b) => a.get('_index') - b.get('_index'));
+    const numberArray = items.map(child => child.get('_isVisited') ? 1 : 0);
+    const activeItem = this.getActiveItem();
+    if (activeItem) numberArray.push(activeItem.get('_index'));
+    this.set('_userAnswer', numberArray);
+  }  
+}
